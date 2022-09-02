567fb5662c5f0b29af8646da8f9c9b26692abd63
Faustinacec <Faustinacec@gmail.com>
2022-09-02 00:53:30 +0000

    check in lesson2 ex1

---
+++ lesson2-ex1/index.js
@@ -0,0 +1,24 @@
+let x = 'FAustina'; // string
+let t = 16; // number
+let b = true; // boolean
+let k = {
+    age: 16,
+    height: '5.35'
+} // object
+let z = [
+    'red',
+    'blue',
+    'yellow'
+] // array
+
+const SayHello = function() {
+    console.log("Hello");
+}
+
+
+console.log(x);
+console.log(t);
+console.log(b);
+console.log(k);
+console.log(z);
+console.log(SayHello());
