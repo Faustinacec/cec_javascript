f4cbdaa4a0a4ae2e41322169fe45f80e825ef482
Faustinacec <Faustinacec@gmail.com>
2022-09-21 00:15:32 +0000

    check in

---
+++ lesson3-assignment/index.js
@@ -0,0 +1,12 @@
+function createImg(){
+    let img = document.createElement("img");
+    img.src = "https://cdn.britannica.com/29/150929-050-547070A1/lion-Kenya-Masai-Mara-National-Reserve.jpg";
+    img.width = 200;
+    document.body.appendChild(img);
+}
+
+function
+ 
+window.onload = function (){
+    createImg();
+}
