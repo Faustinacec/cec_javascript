fc7912adba07a201a839af46e11c2da65abafe01
Faustinacec <Faustinacec@gmail.com>
2022-09-27 23:58:46 +0000

    check in

--- lesson4-assignment/index.js
+++ lesson4-assignment/index.js
@@ -1,1 +1,17 @@
-alert(123);
+function onClick() {
+    // get ranges
+    const bottomRange = document.getElementById("bottom");
+    const topRange = document.getElementById("top");
+
+    submitData(bottomRange.value, topRange.value);
+}
+
+function submitData(bottom, top) {
+    const number = randomNumber(bottom, top);
+    // alert(number);
+   const guess= prompt("guess the number")
+}
+
+function randomNumber(min, max) {
+    return parseInt(Math.random() * (max - min) + min);
+}
